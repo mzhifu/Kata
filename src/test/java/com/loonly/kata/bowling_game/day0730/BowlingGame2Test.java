dd0c2d3128da0063d1029879ab8399e5a4d2a69d
mzhifu <m_zhifu@1633.com>
2019-07-31 00:58:21 +0800

    0730

---
+++ src/test/java/com/loonly/kata/bowling_game/day0730/BowlingGame2Test.java
@@ -0,0 +1,75 @@
+package com.loonly.kata.bowling_game.day0730;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @Author: Loonly
+ * @Date: 2019/7/31 0:11
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BowlingGame2Test {
+  
+  private Game2 g;
+  
+  @Before
+  public void testSetterGame() {
+    g = new Game2();
+  }
+  
+  @Test
+  public void testGetterGame() {
+    rollMany(20, 0);
+    assertEquals(0, g.score());
+  }
+  
+  @Test
+  public void testAllOnes() {
+    rollMany(20, 1);
+    assertEquals(20, g.score());
+  }
+  
+  private void rollMany(int n, int pins) {
+    for (int i = 0; i < n; i++) {
+      g.roll(pins);
+    }
+  }
+  
+  @Test
+  public void testSpare() {
+    rollSpare();
+    g.roll(3);
+   rollMany(17, 0);
+   assertEquals(16, g.score());
+  }
+  
+  private void rollSpare() {
+    g.roll(5);
+    g.roll(5);
+  }
+  
+  @Test
+  public void testStrike() {
+    rollStrike();
+    g.roll(3);
+    g.roll(4);
+    rollMany(16, 0);
+    assertEquals(24, g.score());
+  }
+  
+  private void rollStrike() {
+    g.roll(10);
+  }
+  
+  @Test
+  public void testPerfect() {
+    rollMany(12, 10);
+    assertEquals(300, g.score());
+  }
+}
---
+++ src/test/java/com/loonly/kata/bowling_game/day0730/Game1.java
@@ -0,0 +1,56 @@
+package com.loonly.kata.bowling_game.day0730;
+
+/**
+ * @Author: Loonly
+ * @Date: 2019/7/30 23:03
+ */
+
+public class Game1 {
+  
+  private int[] rolls = new int[21];
+  
+  private int currentRoll = 0;
+  
+  public void roll(int pins) {
+    rolls[currentRoll++] = pins;
+  }
+  
+  public int score() {
+    int score = 0;
+    int frameIndex = 0;
+    for (int frame = 0; frame < 10; frame++) {
+      if (isStrike(frameIndex)) {
+        score += 10 + strikeBonus(frameIndex);
+        frameIndex ++;
+      }else if (isSpare(frameIndex)) {
+        score += 10 + spareBonus(frameIndex);
+        frameIndex += 2;
+      } else {
+        score += sumOfFrame(frameIndex);
+        frameIndex += 2;
+      }
+      
+    }
+    return score;
+  }
+  
+  private int sumOfFrame(int frameIndex) {
+    return rolls[frameIndex] + rolls[frameIndex+1];
+  }
+  
+  private int spareBonus(int frameIndex) {
+    return rolls[frameIndex + 2];
+  }
+  
+  private boolean isSpare(int frameIndex) {
+    return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+  }
+  
+  private int strikeBonus(int frameIndex) {
+    return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+  }
+  
+  private boolean isStrike(int frameIndex) {
+    return rolls[frameIndex] == 10;
+  }
+}
