81f04f51e5d9c898141587afe996c3d3c4f222f4
mzhifu <m_zhifu@1633.com>
2019-08-12 23:40:57 +0800

    0812

---
+++ src/test/java/com/loonly/kata/bowling_game/day0812/Game3.java
@@ -0,0 +1,55 @@
+package com.loonly.kata.bowling_game.day0812;
+
+/**
+ * @Author: Loonly
+ * @Date: 2019/8/12 23:32
+ */
+
+public class Game3 {
+  
+  private int[] arrays = new int[21];
+  
+  private int currentRoll = 0;
+  
+  public void roll(int pins) {
+    arrays[currentRoll++] = pins;
+  }
+  
+  public int score() {
+    int score = 0;
+    int frameIndex = 0;
+    for (int frame = 0; frame < 10; frame++) {
+      if (isStrike(frameIndex)) {
+        score += 10 + strikeBonus(frameIndex);
+        frameIndex ++;
+      } else if (isSpare(frameIndex)) {
+        score += 10 + spareBonus(frameIndex);
+        frameIndex += 2;
+      } else {
+        score += sum(frameIndex);
+        frameIndex += 2;
+      }
+    }
+    return score;
+  }
+  
+  private int sum(int frameIndex) {
+    return arrays[frameIndex] + arrays[frameIndex + 1];
+  }
+  
+  private int spareBonus(int frameIndex) {
+    return arrays[frameIndex + 2];
+  }
+  
+  private int strikeBonus(int frameIndex) {
+    return arrays[frameIndex + 1] + arrays[frameIndex + 2];
+  }
+  
+  private boolean isSpare(int frameIndex) {
+    return arrays[frameIndex] + arrays[frameIndex + 1] == 10;
+  }
+  
+  private boolean isStrike(int frameIndex) {
+    return arrays[frameIndex] == 10;
+  }
+}
