f64a97bf4ec36fde2a8415d5c0ad99845493d676
mzhifu <m_zhifu@1633.com>
2019-09-03 23:08:22 +0800

    0902

---
+++ src/test/java/com/loonly/kata/bowling_game/day0902/BowlingGame2Test.java
@@ -0,0 +1,73 @@
+package com.loonly.kata.bowling_game.day0902;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @Author: Loonly
+ * @Date: 2019/9/2 22:32
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BowlingGame2Test {
+  
+  private Game2 g;
+  
+  @Test
+  public void testGetterGame() {
+    rollMany(20, 0);
+    assertEquals(0, g.score());
+  }
+  
+  
+  @Test
+  public void testAllOne() {
+    int n = 20;
+    int pins = 1;
+    rollMany(n, pins);
+    assertEquals(0, g.score());
+  }
+  
+  private void rollMany(int n, int pins) {
+    for (int i = 0; i < n; i++) {
+      g.roll(pins);
+    }
+  }
+  
+  @Test
+  public void testOneSpare() {
+    rollSpare();
+    g.roll(3);
+    rollMany(17, 0);
+    assertEquals(16, g.score());
+  }
+  
+  private void rollSpare() {
+    g.roll(5);
+    g.roll(5);
+  }
+  
+  @Test
+  public void testOneStrike() {
+    rollStrike();
+    g.roll(3);
+    g.roll(4);
+    rollMany(16, 0);
+    assertEquals(24, g.score());
+  }
+  
+  private void rollStrike() {
+    g.roll(10);
+  }
+  
+  @Test
+  public void testPerfectGame() {
+    rollMany(12, 10);
+    assertEquals(300, g.score());
+  }
+}
