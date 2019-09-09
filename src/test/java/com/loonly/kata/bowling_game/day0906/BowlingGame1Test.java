c79d29de5de7679b9b6269ddc1a5af1c3b161f56
mzhifu <m_zhifu@1633.com>
2019-09-10 00:52:56 +0800

    6 7  8  9

---
+++ src/test/java/com/loonly/kata/bowling_game/day0906/BowlingGame1Test.java
@@ -0,0 +1,78 @@
+package com.loonly.kata.bowling_game.day0906;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @Author: Loonly
+ * @Date: 2019/9/9 23:20
+ */
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class BowlingGame1Test {
+  
+  private Game1  g;
+  
+  @Before
+  public void testSetterGame() {
+    g = new Game1();
+  }
+  
+  @Test
+  public void testGetterGame() {
+    rollMany(20, 0);
+    assertEquals(0, g.score());
+  }
+  
+  @Test
+  public void testAllOne() {
+    int n = 20;
+    int pins = 1;
+    rollMany(n, pins);
+    assertEquals(20, g.score());
+  }
+  
+  private void rollMany(int n, int pins) {
+    for (int i = 0; i < n; i++) {
+      g.roll(pins);
+    }
+  }
+  
+  @Test
+  public void testOneSpare() {
+    rollSpare();
+    g.roll(3);
+    rollMany(17, 0);
+    assertEquals(16, 0);
+  }
+  
+  private void rollSpare() {
+    g.roll(5);
+    g.roll(5);
+  }
+  
+  @Test
+  public void testOneStrike() {
+    rollStrike();
+    g.roll(3);
+    g.roll(4);
+    rollMany(16, 0);
+    assertEquals(24, g.score());
+  }
+  
+  private void rollStrike() {
+    g.roll(10);
+  }
+  
+  @Test
+  public void testPerfectGame() {
+    rollMany(12, 10);
+    assertEquals(300, g.score());
+  }
+}
---
+++ src/test/java/com/loonly/kata/bowling_game/day0907/BowlingGame3Test.java
@@ -0,0 +1,64 @@
+package com.loonly.kata.bowling_game.day0907;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @Author: Loonly
+ * @Date: 2019/9/9 23:59
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class BowlingGame3Test {
+  
+  private Game3 g;
+  
+  @Before
+  public void testSetterGame() {
+    g = new Game3();
+  }
+  
+  @Test
+  public void testGetterGame() {
+    rollMany(20, 0);
+    assertEquals(0, g.score());
+  }
+  
+  @Test
+  public void testAllOnes() {
+    int n = 20;
+    int pins = 1;
+    rollMany(n, pins);
+    assertEquals(20, g.score());
+  }
+  
+  private void rollMany(int n, int pins) {
+    for (int i = 0; i < n; i++) {
+      g.roll(pins);
+    }
+  }
+  
+  @Test
+  public void testOneStrike() {
+    rollStrike();
+    g.roll(3);
+    g.roll(4);
+    rollMany(16, 0);
+    assertEquals(24, g.score());
+  }
+  
+  private void rollStrike() {
+    g.roll(10);
+  }
+  
+  @Test
+  public void testPerfectGame() {
+    rollMany(12, 10);
+    assertEquals(300, g.score());
+  }
+}
